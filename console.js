14d7da94cf1f69e65bbebd4a6ac10ed3a747d30a
Shivam-Sharma1127 <shivamsharma20011127@gmail.com>
2023-08-09 11:16:14 +0530

    shivam sharma

--- console.js
+++ console.js
@@ -1,9 +1,11 @@
 //hey this is a comment
 console.log("hello world")
 console.log("hello world") // 2nd time
+console.log("Shivam Sharma");// shivam
 
 function main(){
   return null;
 }
 main()
-console.log(main)
+console.log(main);
+
